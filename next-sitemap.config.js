ffc2640ddb55383c3242d5238076208ffc2d257a
hamzafallahi <hamzafallahi@esen.tn>
2026-02-14 02:50:37 +0100

    Update sitemap and robots.txt to exclude admin and login pages

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,14 @@
 module.exports = {
   siteUrl: "https://hamzafallahi.me/",
   generateRobotsTxt: true, // Generate robots.txt
+  exclude: ['/admin', '/login'], // <--- Add this line to hide these pages
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/admin', '/login'], // <--- Tells bots not to crawl these
+      },
+    ],
+  },
 };
